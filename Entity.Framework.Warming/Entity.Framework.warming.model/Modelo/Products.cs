0eeb2b741d16de094d1936e0dcc186e458fec99e
Beto Morales <betomorales@outlook.com>
2016-02-20 14:29:02 -0600

    Added files via upload

---
+++ Entity.Framework.Warming/Entity.Framework.warming.model/Modelo/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Framework.warming.model.Modelo
+{
+    class Products : Product
+    {
+
+        public string ListPriceStr { get { return this.ListPrice.ToString(); }} 
+
+        public int AntiquityInYears { get {
+                return ((int)((DateTime.Now - this.SellStartDate).TotalDays));  }
+        }
+
+
+    }
+
+
+    }
+
---
+++ Entity.Framework.Warming/Entity.Framework.warming.model/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Framework.warming.model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
